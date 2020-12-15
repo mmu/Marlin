79dfa5c580b400cb79ab5997c13e0f39c0f5e1d1
Manuel <449667+mmu@users.noreply.github.com>
2020-12-15 12:17:00 +0100

    config for 3D Touch, set 5V mode

--- config/Configuration_adv.h
+++ config/Configuration_adv.h
@@ -21,7 +21,7 @@
  */
 #pragma once
 
-#define CONFIG_EXAMPLES_DIR "Sovol/SV-01"
+//#define CONFIG_EXAMPLES_DIR "Sovol/SV-01"
 
 /**
  * Configuration_adv.h
@@ -710,7 +710,7 @@
    * differs, a mode set eeprom write will be completed at initialization.
    * Use the option below to force an eeprom write to a V3.1 probe regardless.
    */
-  //#define BLTOUCH_SET_5V_MODE
+  #define BLTOUCH_SET_5V_MODE
 
   /**
    * Safety: Activate if connecting a probe with an unknown voltage mode.
